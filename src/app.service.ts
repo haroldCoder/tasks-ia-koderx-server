3c21a260a15e29558e966afee13d2af8bbafdba1
haroldCoder <haroldc2005@gmail.com>
2025-03-16 18:09:01 -0500

    feature: create connection with supabase
    configuration of supabase and service for interactue with this

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { SupabaseService } from './database/supabase.service';
 
 @Injectable()
 export class AppService {
+  constructor(private supabaseService: SupabaseService) {}
   getHello(): string {
+    this.supabaseService.getClient();
     return 'Hello World!';
   }
 }
