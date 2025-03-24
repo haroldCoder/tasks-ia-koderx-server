a6ab1f17c2b312d2210ef5fbe69c7ae28a6c6207
haroldCoder <haroldc2005@gmail.com>
2025-03-23 22:32:53 -0500

    feature: create module for login user
    create user dto, service and controller with swagger, also create new user Exception & install jwt if user exist

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { HttpResponse } from 'src/users/types/http-response';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly jwtService: JwtService) { }
+
+    async login(user: CreateUserDto): Promise<HttpResponse<any>> {
+        return { httpStatus: HttpStatus.OK, response: await this.jwtService.signAsync(user) }
+    }
+}
