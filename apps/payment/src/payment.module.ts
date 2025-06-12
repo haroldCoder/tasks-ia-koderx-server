c679d80b1b157b96c833b2f48d9f5e8e39d82eb8
haroldCoder <haroldc2005@gmail.com>
2025-06-12 15:45:18 -0500

    hotfix: update stripe and supabase libraries
    apply new api version 2025-05-28.basil stripe

--- apps/payment/src/payment.module.ts
+++ apps/payment/src/payment.module.ts
@@ -14,7 +14,7 @@ import Stripe from 'stripe';
       provide: 'STRIPE',
       useFactory: (configService: ConfigService) => {
         return new Stripe(configService.get('STRIPE_SECRET_KEY'), {
-          apiVersion: "2025-03-31.basil"
+          apiVersion: "2025-05-28.basil"
         });
       }, 
       inject: [ConfigService],
